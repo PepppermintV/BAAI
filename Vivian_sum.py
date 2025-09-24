fa039fc4e06b9d49c62cc578709f1238d1c48b79
Vivian <vivianangela010@gmail.com>
2025-09-24 16:03:48 +0800

    new

--- Vivian_sum.py
+++ Vivian_sum.py
@@ -9,23 +9,3 @@
 # 2. Process
 
 # 3. Output
-
-{
-    "Task Template": {
-        "prefix": "viviantask",
-        "body": [
-            "#",
-            "# ${TM_FILENAME_BASE/(^[^_]*)_.*/$1/}, ${CURRENT_YEAR}/${CURRENT_MONTH}/${CURRENT_DATE}",
-            "# File: ${TM_FILENAME}",
-            "# Short description of the task",
-            "#",
-            "",
-            "# 1. Input",
-            "",
-            "# 2. Process",
-            "",
-            "# 3. Output"
-        ],
-    "description": "Insert James' task template"
-    }
-}
