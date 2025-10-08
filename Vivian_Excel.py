68ae28bd06167c76c99332c3ce88bba21f54c7b8
Vivian <vivianangela010@gmail.com>
2025-10-08 16:53:55 +0800

    update

--- Vivian_Excel.py
+++ Vivian_Excel.py
@@ -1,5 +1,17 @@
-
-
+#
+# Vivian, 2025/10/08
+# File: Vivian_Excel.py
+# Short description of the task
+#
 import pandas as pd
-df =pd.read_excel('Financial_Sample.xlsx')
-print (df.head())
+
+# 1. Input
+df =pd.read_excel('Financial_Sample.xlsx','Financial_Sample2.xlsx')
+data= { }
+
+# 2. Process
+
+# 3. Output
+sums=df.select_dtypes(include='number').sum()
+print (sums)
+df_combined=pd.concat([df,sums],ignore_index=True)
