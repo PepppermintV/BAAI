4c5842c838acaba560299f259e513feaa2b56cf6
Vivian <vivianangela010@gmail.com>
2025-10-10 01:45:00 +0800

    trial 2

---
+++ Vivian_Excel copy.py
@@ -0,0 +1,20 @@
+#
+# Vivian, 2025/10/08
+# File: Vivian_Excel.py
+# Short description of the task
+#
+import pandas as pd
+import os
+from glob import glob
+
+# 1. Input
+df = pd.read_excel ('C:/Users/Vivian/OneDrive/Desktop/Business and AI/BAAI-1/Financial_Sample.xlsx')
+# 2. Process
+
+# 3. Output
+sums=df.select_dtypes(include='number').sum()
+# print (sums)
+sums_row = pd.DataFrame([sums])
+df_combined=pd.concat([df,sums],ignore_index=True)
+df_combined.to_excel('your_new_file.xlsx', index=False)
+print(df_combined)
