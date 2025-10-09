4d9711aea751c58e1616b73fbaeba68ae4fb8ecd
Vivian <vivianangela010@gmail.com>
2025-10-10 01:33:16 +0800

    trial 1

--- Vivian_Excel.py
+++ Vivian_Excel.py
@@ -8,14 +8,14 @@ import os
 from glob import glob
 
 # 1. Input
-folder_path ='C:\Users\User\Desktop\Vivian\BAAI'
+folder_path ='C:/Users/Vivian/OneDrive/Desktop/Business and AI/BAAI-1'
 excel_files = [f for f in os.listdir(folder_path) if f.endswith(('.xlsx', '.xls'))]
 
 all_dataframes = []
-    for file_name in excel_files:
-        file_path = os.path.join(folder_path, file_name)
-        df = pd.read_excel(file_path)
-        all_dataframes.append(df)
+for file_name in excel_files:
+    file_path = os.path.join(folder_path, file_name)
+    df = pd.read_excel(file_path)
+    all_dataframes.append(df)
 
 # 2. Process
 
