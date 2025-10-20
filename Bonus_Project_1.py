c1af054e33e57641507b8a30fb48a2d5d7956249
Vivian <vivianangela010@gmail.com>
2025-10-20 23:34:19 +0800

    tempt1

--- Bonus_Project_1.py
+++ Bonus_Project_1.py
@@ -3,4 +3,12 @@ import pandas as pd
 
 #input the file address
 df=pd.read_excel ('C:/Users/Vivian/OneDrive/Desktop/Business and AI/BAAI-2/sales_data.xlsx')
+
+
 print (df)
+print
+for in df
+    if ['Monthly_Sales'] >= ['Sales_Target']
+    print ('Yes')
+    else
+    print ('No')
