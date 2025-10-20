e81e418acd3cea225d1418c396f9f8e83095b4b5
Vivian <vivianangela010@gmail.com>
2025-10-21 00:36:48 +0800

    tempt1

--- Bonus_Project_1.py
+++ Bonus_Project_1.py
@@ -3,12 +3,14 @@ import pandas as pd
 
 #input the file address
 df=pd.read_excel ('C:/Users/Vivian/OneDrive/Desktop/Business and AI/BAAI-2/sales_data.xlsx')
-
-
 print (df)
-print
-for in df
-    if ['Monthly_Sales'] >= ['Sales_Target']
-    print ('Yes')
-    else
-    print ('No')
+
+print("=== Is the employee met target? ===")
+for employee in df:
+#     if ['Monthly_Sales'] >= ['Sales_Target']
+#     target 'Yes'
+#     else
+#     target 'No'
+    print(" ")
+    print(f['Employee_Name']) 
+    print(f['Monthly_Sales'])
