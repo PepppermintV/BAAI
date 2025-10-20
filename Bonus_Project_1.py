a55e0ae17e3c4408d38cc467ff64b34bdad0e128
Vivian <vivianangela010@gmail.com>
2025-10-20 22:48:27 +0800

    test 1

---
+++ Bonus_Project_1.py
@@ -0,0 +1,6 @@
+#import package to read excel file
+import pandas as pd
+
+#input the file address
+df=pd.read_excel ('C:/Users/Vivian/OneDrive/Desktop/Business and AI/BAAI-2/sales_data.xlsx')
+print (df)
