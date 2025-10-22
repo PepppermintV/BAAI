a25c021ab1edb000be385dae30f79969dcec8995
Vivian <vivianangela010@gmail.com>
2025-10-22 15:55:53 +0800

    upt1

--- Vivian_Correlation_Analysis.py
+++ Vivian_Correlation_Analysis.py
@@ -3,9 +3,12 @@
 # File: Vivian_Correlation_Analysis.py
 # Short description of the task
 #
+import pandas as pd
 
 # 1. Input
-
+df = pd.read_csv('Simple_Data.csv')
 # 2. Process
-
+print (df.isnull().sum)
 # 3. Output
+print(f'Data loaded succesfully')
+print(f'Dataset shape: {df.shape}')
