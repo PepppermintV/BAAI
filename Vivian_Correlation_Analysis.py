0baa25d4636c7b1125cd88115068e48900b674bb
Vivian <vivianangela010@gmail.com>
2025-10-22 16:28:45 +0800

    yes1

--- Vivian_Correlation_Analysis.py
+++ Vivian_Correlation_Analysis.py
@@ -4,11 +4,17 @@
 # Short description of the task
 #
 import pandas as pd
-
+from scipy import stats
 # 1. Input
-df = pd.read_csv('Simple_Data.csv')
+df = pd.read_csv('Correlation_Analysis_Data.csv')
+correlation,pvalue = stats.pearsonr(df[Marketing_Spend])
+
 # 2. Process
-print (df.isnull().sum().sum())
+# print (df.isnull().sum())
+# print (df.isnull().sum().sum())
+
 # 3. Output
-# print(f'Data loaded succesfully')
+# print(f'Data loaded succesfully!')
 # print(f'Dataset shape: {df.shape}')
+print (f'Correlation: {correlation}')
+print (f'P Value: {pvalue}')
