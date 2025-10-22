d77196ef6d33d1e0f599811c522296154f5f826d
Vivian <vivianangela010@gmail.com>
2025-10-22 15:58:47 +0800

    up1

--- Vivian_Correlation_Analysis.py
+++ Vivian_Correlation_Analysis.py
@@ -8,7 +8,7 @@ import pandas as pd
 # 1. Input
 df = pd.read_csv('Simple_Data.csv')
 # 2. Process
-print (df.isnull().sum)
+print (df.isnull().sum().sum())
 # 3. Output
-print(f'Data loaded succesfully')
-print(f'Dataset shape: {df.shape}')
+# print(f'Data loaded succesfully')
+# print(f'Dataset shape: {df.shape}')
