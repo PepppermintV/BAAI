5e3253f235393a3e022bf90ea5bc7897c5973973
Vivian <vivianangela010@gmail.com>
2025-10-24 14:23:52 +0800

    test1

--- Vivian_Correlation_Analysis.py
+++ Vivian_Correlation_Analysis.py
@@ -13,22 +13,26 @@ import seaborn as sns
 # 1. Input
 df = pd.read_csv('Correlation_Analysis_Data.csv')
 df.info()
-# print( df.iloc[:,1:6])
-# correlation,pvalue = stats.pearsonr(df['Marketing_Spend'],df['Sales_Revenue'])
+print (df.isnull().sum())
+print (df.isnull().sum().sum())
+correlation_matrix=df.iloc[:,1:6].corr()
+print(correlation_matrix.round(3))
+print( df.iloc[:,1:6])
+correlation,pvalue = stats.pearsonr(df['Marketing_Spend'],df['Sales_Revenue'])
 
 # 2. Process
 # df[].corr()
-# print (df.isnull().sum())
-# print (df.isnull().sum().sum())
-correlation_matrix=df.iloc[:,1:6].corr()
-print(correlation_matrix.round(3))
+
 
 # 3. Output
-# print(f'Data loaded succesfully!')
-# print(f'Dataset shape: {df.shape}')
-# print (f'Correlation: {correlation:.2f}')
-# print (f'P Value: {pvalue}')
+print(f'Data loaded succesfully!')
+print(f'Dataset shape: {df.shape}')
+print (f'Correlation: {correlation:.2f}')
+print (f'P Value: {pvalue}')
 sns.heatmap (correlation_matrix)
 plt.title ('Vivian is the most intelligent person in the world')
 plt.tight_layout()
 plt.show()
+
+print (df.head(5))
+print (df.tail(5))
